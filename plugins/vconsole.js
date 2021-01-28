c12bb931644688a6200be590b9c8516593daca2e
zhoujin <171226Q2>
2021-01-28 15:02:45 +0800

    注释冗余代码

--- plugins/vconsole.js
+++ plugins/vconsole.js
@@ -1,7 +1,6 @@
 const Vconsole =
   process.env.NODE_ENV !== 'prod' ? require('vconsole') : {};
 
-export default ({ app }, inject) => {
-  console.log(app);
+export default ({ app }, inject) => { // eslint-disable-line
   if (process.env.NODE_ENV !== 'prod') inject('vConsole', new Vconsole());
 };

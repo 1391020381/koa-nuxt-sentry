1cc5120763e396b21e75ee52385a74fbeaa44e32
周进 <zhoujin@zhoujindeMacBook-Pro.local>
2021-05-31 11:05:10 +0800

    删除冗余代码

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,13 @@
 <template>
-  <div class="container">
-    ishare-paymentIndex
-  </div>
+  <div class="container">koa-nuxt-sentry</div>
 </template>
 
 <script>
-// http://open-ishare.iask.com.cn/pay/qr?orderNo=B20210121112552M0C175&isAutoRenew=1
 export default {};
 </script>
 
 <style  scoped>
-  .container{
-    font-size:24px;
-  }
+.container {
+  font-size: 24px;
+}
 </style>
